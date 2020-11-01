3b24e18a97baa59b0aaaac8f45899880a99b239c
BoykuhLoL <73744553+BoykuhLoL@users.noreply.github.com>
2020-11-01 16:54:37 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,5 +24,5 @@ else
 fi
 
 # Run the Server
-chmod +x mohaa_lnxded
-./mohaa_lnxded +set dedicated 2 +set sv_maxclients 32 +set net_ip 0.0.0.0 +set net_port {{SERVER_PORT}} +exec server.cfg
+chmod +x /home/cointainer/mohaa_lnxded
+/home/container/mohaa_lnxded +set dedicated 2 +set sv_maxclients 32 +set net_ip 0.0.0.0 +set net_port {{SERVER_PORT}} +exec server.cfg
