a4a075f605733fdbc27e7f31f71f18ea5b686ef4
BoykuhLoL <73744553+BoykuhLoL@users.noreply.github.com>
2020-11-01 15:14:20 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# todo: copy all pk3s in env variable 'MOHCFG_MODS'?
+
+cd /home/container
+wget http://185.224.131.90/MOHAA.zip
+unzip MOHAA.zip
+rm MOHAA.zip
+
+# Replace all config placeholders with environment variables
+sed -Ei "s/cfg.sv_hostname/${MOHCFG_HOSTNAME}/g" server-env.cfg
+sed -Ei "s/cfg.rconpassword/${MOHCFG_RCONPASS}/g" server-env.cfg
+sed -Ei "s/cfg.password/${MOHCFG_PASSWORD}/g" server-env.cfg
+
+# Copy the server config file into place
+cp server-env.cfg main/server.cfg
+
+# Copy realism if required
+if [[ "${MOHCFG_REALISM}" = "true" ]]; then
+  cp paks/User_sbrealism.pk3 main/
+else
+  cp paks/User_sbdefault.pk3 main/
+fi
