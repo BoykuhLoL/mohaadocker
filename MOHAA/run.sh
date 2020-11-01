54790af725cb3e1e34462058cb4b73cf209ada16
BoykuhLoL <73744553+BoykuhLoL@users.noreply.github.com>
2020-11-01 12:54:29 +0100

    Update run.sh

--- MOHAA/run.sh
+++ MOHAA/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # todo: copy all pk3s in env variable 'MOHCFG_MODS'?
 
-chmod 777 *
 # Replace all config placeholders with environment variables
 sed -Ei "s/cfg.sv_hostname/${MOHCFG_HOSTNAME}/g" server-env.cfg
 sed -Ei "s/cfg.rconpassword/${MOHCFG_RCONPASS}/g" server-env.cfg
@@ -16,4 +15,3 @@ if [[ "${MOHCFG_REALISM}" = "true" ]]; then
 else
   cp paks/User_sbdefault.pk3 main/
 fi
-
